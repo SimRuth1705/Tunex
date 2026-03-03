0e9b8fea406fd3c371ba94b6b37929823ff3c92e
Samson Raj <samruth1433@gmail.com>
2026-03-03 21:44:06 +0530

    FIX: Connected real backend URL and updated CORS

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@
 const isProduction = import.meta.env.PROD; // Auto-detects if running on Vercel
 
 export const API_BASE_URL = isProduction 
-  ? "https://tunex-backend-abc1.onrender.com" 
+  ? "https://tunex-backend.onrender.com"
   : "http://localhost:5005";
